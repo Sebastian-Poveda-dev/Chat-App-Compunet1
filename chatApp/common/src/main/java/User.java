cc39b69b7e544ee8301010ef0288c4c016af997c
Sebatian-Poveda-dev <1008109183@u.icesi.edu.co>
2025-10-08 16:33:42 -0500

    starting project

---
+++ chatApp/common/src/main/java/User.java
@@ -0,0 +1,31 @@
+public class User {
+
+    private String userId;
+    private String userName;
+
+    public User(String userId, String userName) {
+        this.userId = userId;
+        this.userName = userName;    
+    }
+
+    public User(String userId) {
+        this.userId = userId;
+        this.userName = "Unknown"; 
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
